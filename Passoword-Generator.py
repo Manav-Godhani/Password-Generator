0b4510cc9ada197f8ba7c423cca88da1526317e2
mnv-godhani <godhanik87@gmail.com>
2025-12-03 11:39:28 +0530

    Alp + Num

--- Passoword-Generator.py
+++ Passoword-Generator.py
@@ -1,1 +1,36 @@
-print("Hello World!")
+# password generator - mnv_godhani 😉
+
+import random
+alp = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+num = ["1","2","3","4","5","6","7","8","9","0"]
+
+def pass_generator(x):
+    print("=================================")
+    print(" 1️⃣  Mix of Alphabets and Numbers")
+    print(" 2️⃣  Only Alphabets")
+    print(" 3️⃣  Only Numbers")
+    print("=================================")
+    choice = int(input("Enter Your requirements :-  :- "))
+
+    if choice == 1:
+        allchar = alp + num
+        print("Generating Password...")
+        for i in range(1,x+1):
+            a = random.choice(allchar)
+            print(a, end="")
+    elif choice == 2:
+        for i in range(1,x+1):
+            a = random.choice(alp)
+            print(a, end="")
+    elif choice == 3:
+        for i in range(1,x+1):
+            a = random.choice(num)
+            print(a, end="")
+
+    
+      
+    
+
+length = int(input("Enter the length of password ! :- "))
+pass_generator(length)
+    
